46db99a416f2ba378235b6878816ed341895b5d5
Fero Volar <alian@alian.info>
2022-02-01 14:18:40 +0100

    First commit

---
+++ etcd_backup.sh
@@ -0,0 +1,5 @@
+ETCDCTL_API=3 etcdctl snapshot save /tmp/etcdBackup.db \
+--endpoints=https://10.0.0.1:2379 \
+--cacert=/etc/kubernetes/pki/etcd/ca.crt \
+--cert=/etc/kubernetes/pki/etcd/server.crt \
+--key=/etc/kubernetes/pki/etcd/server.key
